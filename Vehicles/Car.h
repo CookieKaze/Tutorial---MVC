abe65715a8c25a849904c5dbc2420e1b1bc0a534
Erin Luu <luuerin@gmail.com>
2016-11-05 12:18:58 -0400

    Complete tutorial up till and including the section "Inheritance"

---
+++ Vehicles/Car.h
@@ -0,0 +1,19 @@
+//
+//  Car.h
+//  Vehicles
+//
+//  Created by Erin Luu on 2016-11-05.
+//  Copyright © 2016 Designated Nerd Software. All rights reserved.
+//
+
+#import "Vehicle.h"
+
+@interface Car : Vehicle
+
+@property (nonatomic, assign) BOOL isConvertible;
+@property (nonatomic, assign) BOOL isHatchback;
+@property (nonatomic, assign) BOOL hasSunroof;
+@property (nonatomic, assign) NSInteger numberOfDoors;
+
+
+@end
